4545c28c2ebda70058bee5fefee8bc63d0881959
biancabaltag <103136941+biancabaltag@users.noreply.github.com>
2024-12-12 19:21:40 +0200

    Update main.rs-> am adaugat functia de cautare recursiva

--- recursive_grep/main.rs
+++ recursive_grep/main.rs
@@ -1,16 +1,45 @@
-use std::{fs, io};
+use std::fs;
+use std::io::{self, Read};
+use std::path::Path;
 
-/// Verifică dacă un substring există într-un string, începând de la o anumită poziție.
-fn potrivire_naiva(continut_bytes: &[u8], sir_cautat_bytes: &[u8], start: usize) -> bool {
-    for i in 0..sir_cautat_bytes.len() {
-        if continut_bytes[start + i] != sir_cautat_bytes[i] {
-            return false;
+fn parcurgere_si_cautare(director: &Path, sir_cautat: &str) -> io::Result<()> {
+    if let Ok(optiuni) = fs::read_dir(director) {
+
+        for optiune in optiuni {
+
+            match optiune {
+
+                Ok(entry) => {
+
+                    let path = entry.path();
+
+                    if path.is_dir() {
+
+                        // daca este director apelam recursiv functia
+                        parcurgere_si_cautare(&path, sir_cautat)?;
+
+                    } else if path.is_file() {
+
+                        // daca este fisier, citim continutul si cautam
+                        println!("CAUTAM IN FISIERUL: {}", path.display());
+                        let mut file = fs::File::open(&path)?;
+                        let mut continut_fisier = String::new();
+
+                        file.read_to_string(&mut continut_fisier)?;
+                        cautare(&continut_fisier, sir_cautat);
+                    }
+                }
+                Err(e) => {
+                    eprintln!("Eroare la citirea/directorului fisierului de optiune: {}", e);
+                }
+            }
         }
     }
-    true
+    Ok(())
 }
 
 fn cautare(continut_fisier: &str, sir_cautat: &str) {
+  
     let mut linia_curenta = 1;
     let mut inceput_linie = 0;
     let sir_cautat_len = sir_cautat.len();
@@ -31,7 +60,7 @@ fn cautare(continut_fisier: &str, sir_cautat: &str) {
 
         // verificam prin alg naiv daca exista substring-ul la pozitia respectiva
         if index + sir_cautat_len <= continut_len {
-            if potrivire_naiva(continut_bytes, sir_cautat_bytes, index) {
+            if am_gasit(continut_bytes, sir_cautat_bytes, index) {
                 // daca am gasit o portivere, afisam 
                 let index_in_linie = index - inceput_linie;
                 let sfarsit_linie = continut_fisier[inceput_linie..]
@@ -50,9 +79,19 @@ fn cautare(continut_fisier: &str, sir_cautat: &str) {
     }
 }
 
+fn am_gasit(continut_bytes: &[u8], sir_cautat_bytes: &[u8], start: usize) -> bool {
+    for i in 0..sir_cautat_bytes.len() {
+        if continut_bytes[start + i] != sir_cautat_bytes[i] {
+            return false;
+        }
+    }
+    true
+}
+
 fn main() -> Result<(), io::Error> {
-    let continut = fs::read_to_string("exemplu.txt")?;
-    let sir_cautat = "o";
-    cautare(&continut, sir_cautat);
+    let director = Path::new("text");
+    let sir_cautat = "pe";
+
+    parcurgere_si_cautare(director, sir_cautat)?;
     Ok(())
 }
